438cb5f8294bf812e6737dfa89ec79b05e9189db
jupyter1029 <jupyter1029@gmail.com>
2021-01-28 11:22:26 -0800

    on windows 3.6, set pytest's capture option to sys

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,17 @@
+import sys
+import os
+
 pytest_plugins = [
     "jupyter_server.pytest_plugin"
 ]
+
+
+def pytest_configure(config):
+
+    if all(
+        sys.platform.startswith("win32"),
+        sys.version_info.major == 3,
+        sys.version_info.minor == 6,
+        config.option.capture is None
+    ):
+        config.option.capture == 'sys'
