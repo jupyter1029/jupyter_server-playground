fa3d0f02836e541494eb94bf4226a9eeab90d48f
jupyter1029 <jupyter1029@gmail.com>
2021-01-28 12:13:27 -0800

    get the tests working!

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,3 @@
-import sys
-
-
 pytest_plugins = [
     "jupyter_server.pytest_plugin"
 ]
-
-
-def pytest_configure(config):
-    if all([
-        sys.platform.startswith("win32"),
-        sys.version_info.major == 3,
-        sys.version_info.minor == 6,
-        config.option.capture == 'fd'
-    ]):
-        config.option.capture = 'sys'
